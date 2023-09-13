f7cd51b7af723f3eebac7704cbceabbdc5c40fe7
tracer-ics2023 <tracer@njuics.org>
2023-09-13 23:18:45 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:45 up  5:21,  5 users,  load average: 3.04, 2.35, 1.86

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,16 @@ static int cmd_q(char *args) {
   return -1; 
 }
 
+static int cmd_si(char *args) {
+	int n;
+	char *num=strtok(args," ");
+	if(num==NULL)
+		n=1;
+	else sscanf(num,"%d",&n);
+	cpu_exec(n);
+	return 0;
+} 
+
 static int cmd_help(char *args);
 
 static struct {
@@ -63,6 +73,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Let the program step N instructions", cmd_si }
 
   /* TODO: Add more commands */
 
