c2ecd7a27fa5364a3fe20414b8432a645714fa9e
tracer-ics2023 <tracer@njuics.org>
2023-10-04 17:53:57 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:57 up 2 days, 11:16,  4 users,  load average: 0.55, 0.46, 0.54

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -234,11 +234,9 @@ word_t eval(int p,int q)
 			}
 		}
 		Log("op=%d\n",op);
-		if(pri>=P_LEF||pri==0)return 0;
 		word_t val1=0,val2=0;
 		if(op!=p)val1=eval(p,op-1);
 		if(op!=q)val2=eval(op+1,q);
-		Log("val1 = %u, val2 = %u , val1&&val2=%u\n",val1,val2,val1&&val2);
 		switch (tokens[op].type) {
 			case TK_PLU: return val1+val2;break;
 			case TK_MIN: return val1-val2;break;
