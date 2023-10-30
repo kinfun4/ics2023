a3166c9e6321b13e58238c1f7af17f9ca27326be
tracer-ics2023 <tracer@njuics.org>
2023-10-31 01:11:22 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:11:22 up 2 days,  8:30,  4 users,  load average: 0.35, 0.25, 0.25

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R, TYPE_B,
   TYPE_N, // none
 };
 
@@ -34,6 +34,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1; } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1; } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -46,6 +47,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
     case TYPE_R: src1R(); src2R();         break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
   }
 }
 
@@ -89,6 +91,13 @@ static int decode_exec(Decode *s) {
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2);
   INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, R(rd) = (sword_t)src1 < (sword_t)src2 ? 1 : 0);
   INSTPAT("0000000 ????? ????? 011 ????? 01100 11", sltu   , R, R(rd) = src1 < src2 ? 1 : 0);
+
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, s->snpc = (src1 == src2) ? s->pc + imm : s->snpc);
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, s->snpc = (src1 != src2) ? s->pc + imm : s->snpc);
+  INSTPAT("??????? ????? ????? 100 ????? 11000 11", blt    , B, s->snpc = ((sword_t)src1 < (sword_t)src2) ? s->pc + imm : s->snpc);
+  INSTPAT("??????? ????? ????? 110 ????? 11000 11", bltu   , B, s->snpc = (src1 < src2) ? s->pc + imm : s->snpc);
+  INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge    , B, s->snpc = ((sword_t)src1 >= (sword_t)src2) ? s->pc + imm : s->snpc);
+  INSTPAT("??????? ????? ????? 111 ????? 11000 11", bgeu   , B, s->snpc = (src1 >= src2) ? s->pc + imm : s->snpc);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
