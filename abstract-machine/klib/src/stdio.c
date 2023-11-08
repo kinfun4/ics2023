2b559f1e0e8df1dacb465a6194632e518aeb42c3
tracer-ics2023 <tracer@njuics.org>
2023-11-09 00:40:49 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:40:49 up 2 days, 15:13,  5 users,  load average: 0.66, 0.66, 0.59

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,8 +2,38 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include <stddef.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+void int2str(int num, char *str,int* p) {
+    int i = *p;
+
+    if (num < 0) {
+        num = -num;
+        str[i++] = '-';
+    }
+
+    do {
+        str[i++] = num % 10 + '0';
+        num /= 10;
+    } while (num);
+
+    str[i] = '\0';
+
+    int j = *p;
+
+    if (str[0] == '-') {
+        j++;
+    }
+
+    for ( int k=j; k < (i+j) / 2; k++) {
+        str[k] = str[k] + str[i - 1 - (k-j)];
+        str[i - 1 - (k-j)] = str[k] - str[i - 1 - (k-j)];
+        str[k] = str[k] - str[i - 1 - (k-j)];
+    }
+    *p=i+1;
+}
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
@@ -14,7 +44,35 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  size_t i=0;
+  int ret=0;
+  char *s;size_t j;
+  int d;
+  va_start(ap, fmt);
+  while(fmt[i]!='\0'){
+    if(fmt[i]!='%'){
+      out[ret++]=fmt[i++];
+    }
+    else {
+      switch (fmt[i+1]) {
+        case 's': 
+          s=va_arg(ap, char*);
+          j=0;
+          i=i+2;
+          while(s[j]!='\0'){
+            out[ret++]=s[j++];
+          }
+          break;
+        case 'd':
+          d=va_arg(ap, int);
+          i=i+2;
+          int2str(d, out,&ret);
+      }
+    }
+  }
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
