8ebb5e93a14c8fa26e9039910b6936f51f43015c
tracer-ics2023 <tracer@njuics.org>
2023-11-10 00:05:37 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:37 up 3 days, 14:38,  5 users,  load average: 0.11, 0.20, 0.28

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -138,7 +138,9 @@ void cpu_exec(uint64_t n) {
       break;
 
     case NEMU_END: 
-      if(!nemu_state.halt_ret)print_iringbuf();
+      #ifdef CONFIG_ITRACE_COND
+        if(nemu_state.halt_ret==0)print_iringbuf();
+      #endif /* ifdef CONFIG_ITRACE_COND */
       Log("nemu: %s at pc = " FMT_WORD,
           nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED),
           nemu_state.halt_pc);
