27d0bf2536df56ecbf2c724c305fd7537245f346
tracer-ics2023 <tracer@njuics.org>
2023-11-10 08:57:04 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:57:04 up 3 days, 23:29,  5 users,  load average: 0.47, 0.39, 0.35

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -67,7 +67,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  IFDEF(CONFIG_MTRACE,printf("Write memory : addr = %08x  len = %d  data = %032x\n",addr,len,data));
+  IFDEF(CONFIG_MTRACE,printf("Write memory : addr = %08x  len = %d  data = %08x\n",addr,len,data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
