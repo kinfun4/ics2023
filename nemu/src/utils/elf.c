2a82c1a89b6ef708c637a2d5e6e0193abd4264f0
tracer-ics2023 <tracer@njuics.org>
2023-11-17 11:06:24 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:06:24 up 1 day, 30 min,  5 users,  load average: 1.04, 0.74, 0.48

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -24,14 +24,25 @@ FILE *elf_fp = NULL;
 void init_elf(const char *elf_file) {
   if (elf_file == NULL)return;
 
-  FILE *fp = fopen(elf_file, "rb");
-  Assert(fp, "Can not read '%s'", elf_file);
-  elf_fp = fp;
+  elf_fp = fopen(elf_file, "rb");
+  Assert(elf_fp, "Can not read '%s'", elf_file);
+  FILE *fp =elf_fp;
+
   Elf32_Ehdr header;
-  if(fread(&header, sizeof(Elf32_Ehdr), 1, elf_fp)!=1){
-    Assert(0, "Can not read '%s'",elf_file);
+  if(fread(&header, sizeof(Elf32_Ehdr), 1, fp)!=1){
+    Assert(0, "Can not read ELF Header");
   }
   printf("%d,%d,%d\n",header.e_shoff,header.e_shentsize,header.e_shnum);
+  Assert(header.e_shstrndx==SHN_UNDEF, "There is no String and Symble Table ");
+
+  fseek(fp, header.e_shoff + header.e_shstrndx*header.e_shentsize, SEEK_SET);
+  Elf32_Shdr str_table;
+  Assert(fread(&str_table, sizeof(Elf32_Shdr), 1, elf_fp)==1, "Can not read Section Table");
+  
+  fseek(fp, str_table.sh_offset, SEEK_SET);
+  char * symble = alloca(sizeof(char)*20);
+  Assert(fscanf(fp, "%s", symble)!=EOF,"Can not read symble!");
+  printf("%s\n",symble);
 }
 
 bool elf_enable() {
