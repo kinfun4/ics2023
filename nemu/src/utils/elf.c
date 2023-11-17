eaa125661fccfe31a8edd8fa797b2ff02e0f454f
tracer-ics2023 <tracer@njuics.org>
2023-11-17 11:15:17 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:17 up 1 day, 39 min,  5 users,  load average: 0.47, 0.40, 0.40

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -40,9 +40,10 @@ void init_elf(const char *elf_file) {
   Assert(fread(&str_table, sizeof(Elf32_Shdr), 1, elf_fp)==1, "Can not read Section Table");
   
   fseek(fp, str_table.sh_offset, SEEK_SET);
-  char * symble = alloca(sizeof(char)*20);
-    Assert(fscanf(fp, "%s", symble)!=EOF,"Can not read symble!");
-    printf("%s\n",symble);
+  // char * symble = alloca(sizeof(char)*20);
+  //   Assert(fscanf(fp, "%s", symble)!=EOF,"Can not read symble!");
+  //   printf("%s\n",symble);
+  return;
 }
 
 bool elf_enable() {
