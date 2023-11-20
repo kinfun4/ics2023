e7b8b6adfb01ef9c436cf78297c93d56bd3b3a10
tracer-ics2023 <tracer@njuics.org>
2023-11-20 13:00:29 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:00:29 up 13:59,  5 users,  load average: 0.25, 0.24, 0.32

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,77 +1,78 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 #include <stddef.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-void int2str(int num, char *str,int* p) {
-    int i = *p;
+void int2str(int num, char *str, int *p) {
+  int i = *p;
 
-    if (num < 0) {
-        num = -num;
-        str[i++] = '-';
-    }
+  if (num < 0) {
+    num = -num;
+    str[i++] = '-';
+  }
 
-    do {
-        str[i++] = num % 10 + '0';
-        num /= 10;
-    } while (num);
+  do {
+    str[i++] = num % 10 + '0';
+    num /= 10;
+  } while (num);
 
-    int j = *p;
+  int j = *p;
 
-    if (str[j] == '-') {
-        j++;
-    }
+  if (str[j] == '-') {
+    j++;
+  }
 
-    for ( int k=j; k < (i+j) / 2; k++) {
-        str[k] = str[k] + str[i - 1 - (k-j)];
-        str[i - 1 - (k-j)] = str[k] - str[i - 1 - (k-j)];
-        str[k] = str[k] - str[i - 1 - (k-j)];
-    }
-    *p=i;
+  for (int k = j; k < (i + j) / 2; k++) {
+    str[k] = str[k] + str[i - 1 - (k - j)];
+    str[i - 1 - (k - j)] = str[k] - str[i - 1 - (k - j)];
+    str[k] = str[k] - str[i - 1 - (k - j)];
+  }
+  *p = i;
 }
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+int printf(const char *fmt, ...) { panic("Not implemented"); }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t i = 0;
+  int ret = 0;
+  char *s;
+  size_t j;
+  int d;
+  while (fmt[i] != '\0') {
+    if (fmt[i] != '%') {
+      out[ret++] = fmt[i++];
+    } else {
+      switch (fmt[i + 1]) {
+      case 's':
+        s = va_arg(ap, char *);
+        j = 0;
+        i = i + 2;
+        while (s[j] != '\0') {
+          out[ret++] = s[j++];
+        }
+        break;
+      case 'd':
+        d = va_arg(ap, int);
+        i = i + 2;
+        int2str(d, out, &ret);
+        break;
+      default:
+        i = i + 2;
+        break;
+      }
+    }
+  }
+  out[ret] = '\0';
+  return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
-  size_t i=0;
-  int ret=0;
-  char *s;size_t j;
-  int d;
   va_start(ap, fmt);
-  while(fmt[i]!='\0'){
-    if(fmt[i]!='%'){
-      out[ret++]=fmt[i++];
-    }
-    else {
-      switch (fmt[i+1]) {
-        case 's': 
-          s=va_arg(ap, char*);
-          j=0;
-          i=i+2;
-          while(s[j]!='\0'){
-            out[ret++]=s[j++];
-          }
-          break;
-        case 'd':
-          d=va_arg(ap, int);
-          i=i+2;
-          int2str(d, out,&ret);
-          break;
-        default:i=i+2;break;
-      }
-    }
-  }
-  out[ret]='\0';
+  int ret=vsprintf(out,fmt,ap);
   va_end(ap);
   return ret;
 }
