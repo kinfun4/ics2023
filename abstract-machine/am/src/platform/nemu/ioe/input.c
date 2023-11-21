e566db37f0f9a004c0825479835b2ae063249575
tracer-ics2023 <tracer@njuics.org>
2023-11-21 19:53:08 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:53:08 up 1 day, 20:51,  4 users,  load average: 0.64, 0.79, 0.93

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keycode = inl(KBD_ADDR);
+  kbd->keycode = inw(KBD_ADDR);
   kbd->keydown = (kbd->keycode & KEYDOWN_MASK) ? 1 : 0;
 }
