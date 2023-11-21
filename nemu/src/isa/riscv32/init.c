7e71423491b6220bc12eac8eeeef13554ddebf5a
tracer-ics2023 <tracer@njuics.org>
2023-11-21 14:46:47 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:46:47 up 1 day, 15:45,  5 users,  load average: 0.27, 0.21, 0.28

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -19,11 +19,33 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-  0x00000297,  // auipc t0,0
-  0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu a0,16(t0)
-  0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data
+  // 0x00000297,  // auipc t0,0
+  // 0x00028823,  // sb  zero,16(t0)
+  // 0x0102c503,  // lbu a0,16(t0)
+  // 0x00100073,  // ebreak (used as nemu_trap)
+  // 0xdeadbeef,  // some data
+0x00000413,
+0x00009117,
+0xffc10113,
+0x024000ef,
+0x00000717,
+0x04470713,
+0x00072503,
+0x0001f7b7,
+0x94278793,
+0x00f50533,
+0x00a72023,
+0x00008067,
+0xff010113,
+0x00000517,
+0x01c50513,
+0x00112623,
+0xfd1ff0ef,
+0x00050513,
+0x00100073,
+0x0000006f,
+0x00000000,
+0x00000000
 };
 
 static void restart() {
