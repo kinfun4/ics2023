99d7e7e850e121ce3fbd74d6f003a391eb70a713
tracer-ics2023 <tracer@njuics.org>
2023-11-23 00:35:37 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:35:37 up  6:00,  5 users,  load average: 0.18, 0.20, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,36 +4,36 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 #ifdef MODE_800x600
-# define W    800
-# define H    600
+#define W 800
+#define H 600
 #else
-# define W    400
-# define H    300
+#define W 400
+#define H 300
 #endif
 
-void __am_gpu_init() {
-  int i;
-  int w= W;
-  int h = H;
-  uint32_t *fb = (uint32_t *) (uintptr_t) FB_ADDR;
-  for (i=0; i<w*h; i++) fb[i]=i;
-  outl(SYNC_ADDR,1);
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = W, .height = H,
-    .vmemsz = 0
-  };
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = W,
+                           .height = H,
+                           .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w,h=ctl->h;
+  int i,j;
+  int offset_addr;
+  for(i=0;i<h;i++)
+    for (j=0;j<w;j++) {
+      offset_addr = (y+i)*W+(x+j);
+      outl(FB_ADDR+ offset_addr, *(uint32_t *)(ctl->pixels + offset_addr));
+    }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
