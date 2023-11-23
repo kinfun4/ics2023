4b410c07d67d124e4fcd89e3f2309279645438b5
tracer-ics2023 <tracer@njuics.org>
2023-11-23 22:37:06 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:37:06 up 1 day,  4:01,  5 users,  load average: 0.36, 0.37, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -9,20 +10,24 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+static uint32_t offset_addr=0;
+static uint32_t bufsize=0;
+
 void __am_audio_init() {
-  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  offset_addr = 0;
+  bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  __am_audio_init();
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
@@ -30,4 +35,15 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  uint8_t *buf = ctl->buf.start;
+  int nwrite = 0;
+  int count = inl(AUDIO_SBUF_SIZE_ADDR);
+  while(count<bufsize && nwrite < len){
+    outb(AUDIO_SBUF_ADDR + offset_addr, *(buf+ nwrite));
+    offset_addr = (offset_addr + 1) % bufsize;
+    nwrite++;
+    count++;
+  }
+  outl(AUDIO_COUNT_ADDR, count);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,22 +1,26 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_audio.h>
+#include <SDL2/SDL_stdinc.h>
+#include <assert.h>
 #include <common.h>
 #include <device/map.h>
 #include <device/mmio.h>
-#include <SDL2/SDL.h>
+#include <sys/types.h>
 
 enum {
   reg_freq,
@@ -30,19 +34,63 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static uint8_t silence = 0;
+static uint32_t offset_addr = 0;
+
+static void audio_play(void *userdata, uint8_t *stream, int len) {
+  int nread = 0;
+  int count = audio_base[reg_count];
+  while (nread < len && count > 0) {
+    *(stream + nread) = *(sbuf + offset_addr);
+    offset_addr = (offset_addr + 1) % CONFIG_SB_SIZE;
+    nread++;
+    count--;
+  }
+  audio_base[reg_count] = count;
+  if (nread < len)
+    memset(stream + nread, silence, len - nread);
+};
+
+static void init_audio_ctrl() {
+  SDL_AudioSpec s;
+  SDL_zero(s);
+  s.format = AUDIO_S16SYS;
+  s.userdata = NULL;
+  s.freq = audio_base[reg_freq];
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  s.callback = audio_play;
+  assert(SDL_InitSubSystem(SDL_INIT_AUDIO) == 0);
+  assert(SDL_OpenAudio(&s, NULL) == 0);
+  silence = s.silence;
+  SDL_PauseAudio(0);
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if (audio_base[reg_init] == 1) {
+    init_audio_ctrl();
+    audio_base[reg_init] = 0;
+  }
+  if(offset == reg_count && !is_write){
+    SDL_LockAudio();
+  }
+  if(offset == reg_count && is_write){
+    SDL_UnlockAudio();
+  }
 }
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+  add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size,
+              audio_io_handler);
 #else
-  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
+  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size,
+               audio_io_handler);
 #endif
-
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  offset_addr =0;
 }
