b5acc1da69f282509f6e6a5168b28d1787775003
tracer-ics2023 <tracer@njuics.org>
2023-11-24 01:13:13 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:13:13 up 1 day,  6:37,  5 users,  load average: 0.40, 0.54, 0.61

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -43,7 +43,6 @@ static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
 static void init_screen() {
-  printf("1\n");
   SDL_Window *window = NULL;
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
