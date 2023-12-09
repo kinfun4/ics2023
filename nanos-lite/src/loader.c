e01eeb725eae9b115362ddd1022433549cc32f9a
tracer-ics2023 <tracer@njuics.org>
2023-12-10 01:30:46 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:30:46 up  1:32,  6 users,  load average: 0.16, 0.21, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,45 +9,44 @@
 #define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__)
+#define EXPECT_TYPE EM_RISCV
+#else
+#error Unsupported ISA
+#endif
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf32_Ehdr Ehdr[1];
+  Elf_Ehdr Ehdr[1];
   ramdisk_read(Ehdr, 0, sizeof(Elf32_Ehdr));
 
-  assert(Ehdr[0].e_ident[EI_MAG0] == ELFMAG0);
-  assert(Ehdr[0].e_ident[EI_MAG1] == ELFMAG1);
-  assert(Ehdr[0].e_ident[EI_MAG2] == ELFMAG2);
-  assert(Ehdr[0].e_ident[EI_MAG3] == ELFMAG3);
+  assert(Ehdr->e_ident[EI_MAG0] == ELFMAG0);
+  assert(Ehdr->e_ident[EI_MAG1] == ELFMAG1);
+  assert(Ehdr->e_ident[EI_MAG2] == ELFMAG2);
+  assert(Ehdr->e_ident[EI_MAG3] == ELFMAG3);
 
-  Elf32_Addr e_entry = Ehdr[0].e_entry;
-  Elf32_Off e_phoff = Ehdr[0].e_phoff;
-  uint16_t e_phnum = Ehdr[0].e_phnum;
-  uint16_t e_phentsize = Ehdr[0].e_phentsize;
+  assert(Ehdr->e_phoff != 0 && Ehdr->e_phnum != 0);
 
-  assert(e_phoff != 0 && e_phnum != 0);
+  Elf_Phdr Phdr[1];
+  for (int i = 0; i < Ehdr->e_phnum; i++) {
+    ramdisk_read(Phdr, Ehdr->e_phoff + i * Ehdr->e_phentsize,
+                 Ehdr->e_phentsize);
 
-  Elf32_Phdr Phdr[1];
-  for (int i = 0; i < e_phnum; i++) {
-    ramdisk_read(Phdr, e_phoff + i * e_phentsize, e_phentsize);
-
-    if (Phdr[0].p_type == PT_LOAD) {
-      Elf32_Off p_offset = Phdr[0].p_offset;
-      Elf32_Addr p_vaddr = Phdr[0].p_vaddr;
-      uint32_t p_filesz = Phdr[0].p_filesz;
-      uint32_t p_memsz = Phdr[0].p_memsz;
-      char buf[p_filesz];
-
-      ramdisk_read(buf, p_offset, p_filesz);
-      memcpy((void *)p_vaddr, buf, p_filesz);
-
-      if (p_memsz > p_filesz)
-        memset((void *)p_vaddr + p_filesz, 0, p_memsz - p_filesz);
+    if (Phdr->p_type == PT_LOAD) {
+      char buf[Phdr->p_filesz];
+      ramdisk_read(buf, Phdr->p_offset, Phdr->p_filesz);
+      memcpy((void *)Phdr->p_vaddr, buf, Phdr->p_filesz);
+      if (Phdr->p_memsz > Phdr->p_filesz)
+        memset((void *)Phdr->p_vaddr + Phdr->p_filesz, 0,
+               Phdr->p_memsz - Phdr->p_filesz);
     }
   }
 
-  return e_entry;
+  return Ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
