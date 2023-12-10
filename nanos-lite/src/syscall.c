eb01b82b881090f3af7ade88eaeb2693096b73af
tracer-ics2023 <tracer@njuics.org>
2023-12-10 13:10:15 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:10:15 up 13:12,  5 users,  load average: 1.26, 1.22, 1.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,12 @@
 #include <common.h>
 #include "syscall.h"
+#include "am.h"
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_exit: halt(c->GPR2); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
