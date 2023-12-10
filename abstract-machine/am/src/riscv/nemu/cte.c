22c4e7a085c5956aca716ce6c288944dae4de59e
tracer-ics2023 <tracer@njuics.org>
2023-12-10 13:08:31 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:08:31 up 13:10,  5 users,  load average: 1.43, 1.24, 1.19

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xb: ev.event = c->GPR1; c->mepc +=4; break;
+      case 0xb: ev.event = EVENT_SYSCALL; c->mepc +=4; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,9 +2,11 @@
 #include <common.h>
 #include <stdio.h>
 
+void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("EVENT_YIELD\n");break;
+    case EVENT_SYSCALL: do_syscall(c);break;
     case EVENT_ERROR: panic("EVENT_ERROR\n");break;
     default: panic("Unhandled event ID = %d", e.event);break;
   }
