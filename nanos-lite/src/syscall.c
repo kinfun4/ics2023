b2901f530b0737b3d9272c50eefadeb6d8c92262
tracer-ics2023 <tracer@njuics.org>
2023-12-10 13:32:16 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:32:16 up 13:34,  5 users,  load average: 1.28, 1.27, 1.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,12 +4,17 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
-  printf("GPR2=%d\n",c->GPR2);
+  printf("Before Syscall: ID = %d, GPR2 = %d, GPR3 = %d, GPR4 = %d\n", a[0], a[1], a[2], a[3]);
 
   switch (a[0]) {
     case SYS_exit: halt(c->GPR2); break;
     case SYS_yield: yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  printf("After Syscall: ID = %d, GPRX = %d\n", a[0], c->GPRx);
 }
