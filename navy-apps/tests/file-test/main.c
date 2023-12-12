bd0c23047640f2033e9fe90cd6af09656c54b2be
tracer-ics2023 <tracer@njuics.org>
2023-12-12 19:09:16 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:09:16 up 1 day, 18:03,  5 users,  load average: 0.77, 0.41, 0.30

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -9,13 +9,13 @@ int main() {
   long size = ftell(fp);
   assert(size == 5000);
 
-  printf("1\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+  printf("1\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
