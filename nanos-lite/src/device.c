0668e85e97f671275e761a0a80cc82327e24b8e8
tracer-ics2023 <tracer@njuics.org>
2023-12-12 19:44:11 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:44:11 up 1 day, 18:38,  5 users,  load average: 0.29, 0.36, 0.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,13 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  char *_buf = (char *)buf;
+  size_t ret = 0;
+  while (ret < len) {
+    putch(_buf[ret]);
+    ret++;
+  }
+  return ret;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -5,6 +5,10 @@ typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t serial_write(const void *buf, size_t offset, size_t len);
+size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -28,22 +32,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t std_write(const void *buf, size_t offset, size_t len) {
-  char *_buf = (char *)buf;
-  size_t ret = 0;
-  while (ret < len) {
-    putch(_buf[ret]);
-    ret++;
-  }
-  return ret;
-}
-
 /* This is the information about all files in disk. */
 static int file_cnt;
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, std_write},
-    [FD_STDERR] = {"stderr", 0, 0, invalid_read, std_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
