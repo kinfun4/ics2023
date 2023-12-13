cb7e4cfb446da7e30ae38f14adc3ebd9763fe2f3
tracer-ics2023 <tracer@njuics.org>
2023-12-13 19:16:59 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:16:59 up 11:11,  6 users,  load average: 1.32, 0.56, 0.46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -20,7 +20,7 @@ typedef struct {
 
 size_t *file_offset;
 
-enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_DISPINFO, FD_FB };
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
