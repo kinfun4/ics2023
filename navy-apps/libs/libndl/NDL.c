a98c92d5ee0e74a3f614d7a478b88889ccff7c3d
tracer-ics2023 <tracer@njuics.org>
2023-12-13 16:32:21 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:32:21 up  8:26,  6 users,  load average: 0.47, 0.41, 0.41

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -17,6 +17,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   FILE* fd = fopen("/dev/events", "r");
+    printf("1\n");
   int ret = fread(buf, sizeof(char), len, fd);
   return ret;
 }
