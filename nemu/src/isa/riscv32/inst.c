dafcb37cb3081bd1ae8f491c13f62d3518d3891f
tracer-ics2023 <tracer@njuics.org>
2023-12-13 09:42:02 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:42:02 up  1:36,  5 users,  load average: 0.82, 0.64, 0.49

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,7 +138,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, R(rd) = CSR(imm), CSR(imm) = CSR(imm) & (~src1));
 
   // Trap-Return Instructions
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(MEPC));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(MEPC), RET(s->pc,s->dnpc));
 
 
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = INTR(0xb, s->pc)); //0xb is Machine Mode Environment call
