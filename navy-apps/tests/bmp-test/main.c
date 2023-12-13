a1fdec65963c13af31947b389bf530cc9f88f75a
tracer-ics2023 <tracer@njuics.org>
2023-12-13 20:40:05 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:40:05 up 12:34,  6 users,  load average: 0.80, 0.62, 0.49

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -11,8 +11,7 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  NDL_OpenCanvas(&w, &h);
-  NDL_DrawRect(bmp, (width-w)/2, (height-h)/2, w, h);
+  NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
