c71e982c1c8ca888be3f87197839fcd0ad45b859
tracer-ics2023 <tracer@njuics.org>
2023-12-13 08:17:45 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:17:45 up 12 min,  5 users,  load average: 0.47, 0.35, 0.20

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,10 +57,10 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
 #ifdef CONFIG_DTRACE
   printf("Read Device :%10s    len=%-4d addr=%#x\n",map->name,len,addr);
 #endif /* ifdef CONFIG_DTRACE */
+  check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
@@ -69,10 +69,10 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
 #ifdef CONFIG_DTRACE
   printf("Write Devtce:%10s    len=%-4d addr=%#x\n",map->name,len,addr);
 #endif /* ifdef CONFIG_DTRACE */
+  check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
