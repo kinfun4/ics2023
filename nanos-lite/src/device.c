2a3af8acbcc754a0b0c397b48b63dd622a068850
tracer-ics2023 <tracer@njuics.org>
2023-12-13 17:24:22 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:24:22 up  9:18,  6 users,  load average: 0.42, 0.55, 0.53

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,19 +1,15 @@
-#include "klib-macros.h"
 #include <common.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
-# define MULTIPROGRAM_YIELD() yield()
+#define MULTIPROGRAM_YIELD() yield()
 #else
-# define MULTIPROGRAM_YIELD()
+#define MULTIPROGRAM_YIELD()
 #endif
 
-#define NAME(key) \
-  [AM_KEY_##key] = #key,
+#define NAME(key) [AM_KEY_##key] = #key,
 
-static const char *keyname[256] __attribute__((used)) = {
-  [AM_KEY_NONE] = "NONE",
-  AM_KEYS(NAME)
-};
+static const char *keyname[256]
+    __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   char *_buf = (char *)buf;
@@ -26,23 +22,24 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  char *_buf = buf;
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD); 
-  if(ev.keycode == AM_KEY_NONE)return 0;
-  if(ev.keydown) strcpy(_buf, "kd ");
-  else strcpy(_buf, "ku ");
-  strcpy(_buf+sizeof(_buf), keyname[ev.keycode]);
-  strcpy(_buf+sizeof(_buf), "\n");
-  return sizeof(_buf);
+  char *_buf = (char *)buf;
+  int ret = 0;
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE)
+    return 0;
+  if (ev.keydown)
+    strcpy(_buf, "kd ");
+  else
+    strcpy(_buf, "ku ");
+  // strcpy(_buf + 3, keyname[ev.keycode]);
+  while (_buf[ret] != '\0')
+    ret++;
+  return ret;
 }
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
-}
+size_t dispinfo_read(void *buf, size_t offset, size_t len) { return 0; }
 
-size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
-}
+size_t fb_write(const void *buf, size_t offset, size_t len) { return 0; }
 
 void init_device() {
   Log("Initializing devices...");
