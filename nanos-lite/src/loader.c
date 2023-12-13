06facefc8fdeb5f142407c2dd02e5878f08baa9f
tracer-ics2023 <tracer@njuics.org>
2023-12-13 19:51:09 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:51:09 up 11:45,  7 users,  load average: 0.36, 0.50, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -44,7 +44,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
     if (Phdr->p_type == PT_LOAD) {
       char *buf = malloc(Phdr->p_filesz);
-      printf("buf = %x\n", sizeof(buf));
+      printf("buf = %x\n", Phdr->p_filesz);
       fs_lseek(fd, Phdr->p_offset, SEEK_SET);
       fs_read(fd, buf, Phdr->p_filesz);
       memcpy((void *)Phdr->p_vaddr, buf, Phdr->p_filesz);
