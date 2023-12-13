5530ffa441a6120e963362b41398c6703323fd87
tracer-ics2023 <tracer@njuics.org>
2023-12-13 16:57:03 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:57:03 up  8:51,  6 users,  load average: 0.34, 0.34, 0.41

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,6 +1,7 @@
 #include "klib-macros.h"
 #include <common.h>
 #include <stdio.h>
+#include <string.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
 # define MULTIPROGRAM_YIELD() yield()
@@ -28,6 +29,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   char *_buf = buf;
+  memset(buf, 0, sizeof(buf));
   int ret = 0;
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD); 
   if(ev.keycode == AM_KEY_NONE)return 0;
