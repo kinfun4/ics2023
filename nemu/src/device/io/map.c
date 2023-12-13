a4e632db86c96584c73915be884e15a192d2df61
tracer-ics2023 <tracer@njuics.org>
2023-12-13 08:40:15 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:40:15 up 34 min,  5 users,  load average: 0.23, 0.30, 0.26

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,7 @@ void init_map() {
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
 #ifdef CONFIG_DTRACE
-  printf("Read Device :%10s    len=%-4d addr=%#x\n",map->name,len,addr);
+  // printf("Read Device :%10s    len=%-4d addr=%#x\n",map->name,len,addr);
 #endif /* ifdef CONFIG_DTRACE */
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,7 +18,6 @@
 #include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
-#include <stdio.h>
 
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
