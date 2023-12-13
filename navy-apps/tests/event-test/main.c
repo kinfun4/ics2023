c27bdbcc4c9ae251dd261b70ccd77d9dca70b296
tracer-ics2023 <tracer@njuics.org>
2023-12-13 18:33:53 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:33:53 up 10:28,  6 users,  load average: 0.49, 0.81, 0.75

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -4,9 +4,9 @@
 int main() {
   NDL_Init(0);
   char buf[64];
+      printf("buf = %p\n",buf);
   while (1) {
     if (NDL_PollEvent(buf, sizeof(buf))) {
-      printf("buf = %p\n",buf);
       printf("receive event: %s\n", buf);
     }
   }
