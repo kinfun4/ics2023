28975a2369ec3343fe96c8db9888751924adff49
tracer-ics2023 <tracer@njuics.org>
2023-12-13 08:32:11 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:32:11 up 26 min,  5 users,  load average: 0.42, 0.33, 0.22

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,8 +58,9 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("1\n");
 #ifdef CONFIG_DTRACE
-  printf("Read Device :%10s    len=%-4d addr=%#x\n",map->name,len,addr);
+  // printf("Read Device :%10s    len=%-4d addr=%#x\n",map->name,len,addr);
 #endif /* ifdef CONFIG_DTRACE */
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
