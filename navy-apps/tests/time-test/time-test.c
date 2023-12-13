457e7b407c3caa955ecc3cbb19e43f08a5abe993
tracer-ics2023 <tracer@njuics.org>
2023-12-13 09:21:58 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:21:58 up  1:16,  5 users,  load average: 0.51, 0.56, 0.42

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -4,7 +4,8 @@ int main() {
   uint32_t rec = NDL_GetTicks(); 
   while(1){
     uint32_t new = NDL_GetTicks();
-    if(new - rec > 500000){
+    printf("time = %d\n", new);
+    if(new - rec > 500){
       rec = new;
       printf("Hello!\n");
     }
