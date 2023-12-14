3a8c3ba6265f9266f24ce86ba576cf7ae14b6c07
tracer-ics2023 <tracer@njuics.org>
2023-12-14 19:56:22 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:56:22 up  3:50,  4 users,  load average: 0.37, 0.34, 0.28

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
 #include <fixedptc.h>
-#define CHECK(x,y) assert((int)(x) == (int)(y))
-#define PRINT(x,y) printf("get = %d, want = %d\n", (int)x, (int)(y))
-#define INIT(x,y) \
+#define ASSERT(x, y) assert((int)(x) == (int)(y))
+#define PRINT(x, y) printf("get = %d, want = %d\n", (int)x, (int)(y))
+#define INT(x) fixedpt_toint(x)
+#define INIT(x, y) \
   fixedpt a = fixedpt_rconst(x);\
-  fixedpt b = fixedpt_rconst(y);
+  fixedpt b = fixedpt_rconst(y);\
+  int ia
 
 int main() {
-  INIT(3.2, -3.2)
-  int ia;
-  ia =  fixedpt_toint(fixedpt_muli(a, 3));
+  INIT(3.2, -3.2);
+  ia =  INT(fixedpt_muli(a, 3));
   assert(ia == (int)(3.2 * 3));
-  ia =  fixedpt_toint(fixedpt_mul(a, b));
+  ia =  INT(fixedpt_mul(a, b));
   PRINT(ia , (int)(3.2 * -3.2));
   assert(ia == (int)(3.2 * -3.2));
-  ia =  fixedpt_toint(fixedpt_divi(a, 2));
+  ia =  INT(fixedpt_divi(a, 2));
   assert(ia == (int)3.2 / 2);
-  ia =  fixedpt_toint(fixedpt_div(a, b));
+  ia =  INT(fixedpt_div(a, b));
   assert(ia == (int)(3.2 / -3.2));
-  ia =  fixedpt_toint(fixedpt_floor(a));
+  ia =  INT(fixedpt_floor(a));
   assert(ia == 3);
-  ia =  fixedpt_toint(fixedpt_floor(b));
+  ia =  INT(fixedpt_floor(b));
   assert(ia == -4);
-  ia =  fixedpt_toint(fixedpt_ceil(a));
+  ia =  INT(fixedpt_ceil(a));
   assert(ia == 4);
-  ia =  fixedpt_toint(fixedpt_ceil(b));
-  PRINT(ia , -3);
+  ia =  INT(fixedpt_ceil(b));
   assert(ia == -3);
   printf("PASS\n");
   return 0;
