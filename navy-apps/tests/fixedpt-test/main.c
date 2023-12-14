bf2607b7fa16cb75c9e8f7ed34a3ec7e08bc3dff
tracer-ics2023 <tracer@njuics.org>
2023-12-14 17:41:58 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:41:58 up  1:35,  4 users,  load average: 0.19, 0.20, 0.24

---
+++ navy-apps/tests/fixedpt-test/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <fixedptc.h>
+int main() {
+  fixedpt a = fixedpt_rconst(3.2);
+  fixedpt b = fixedpt_fromint(-3.2);
+  int ia;
+  ia =  fixedpt_toint(fixedpt_muli(a, 3));
+  assert(ia == (int)3.2 * 3);
+  ia =  fixedpt_toint(fixedpt_mul(a, b));
+  assert(ia == (int)3.2 * -3.2);
+  ia =  fixedpt_toint(fixedpt_divi(a, 2));
+  assert(ia == (int)3.2 / 2);
+  printf("PASS\n");
+  return 0;
+}
