c4f05fcad11516feae90513453ebc43975db0bd6
tracer-ics2023 <tracer@njuics.org>
2023-12-14 17:47:03 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:47:03 up  1:40,  4 users,  load average: 0.25, 0.23, 0.24

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include <fixedptc.h>
+#define CHECK(a,b) assert((int)(a) == (int)(b));
+#define PRINT(a,b) printf("get = %d, want = %d\n", (int)a, (int)(b));
 int main() {
   fixedpt a = fixedpt_rconst(3.2);
   fixedpt b = fixedpt_fromint(-3.2);
@@ -8,10 +10,11 @@ int main() {
   ia =  fixedpt_toint(fixedpt_muli(a, 3));
   assert(ia == (int)3.2 * 3);
   ia =  fixedpt_toint(fixedpt_mul(a, b));
-  printf("get = %d, want = %d\n", ia, (int)(3.2 * -3.2));
+  PRINT(ia, 3.2 * -3.2)
   assert(ia == (int)(3.2 * -3.2));
   ia =  fixedpt_toint(fixedpt_divi(a, 2));
   assert(ia == (int)3.2 / 2);
   printf("PASS\n");
   return 0;
 }
+
