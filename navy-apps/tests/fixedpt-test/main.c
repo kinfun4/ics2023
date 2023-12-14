f8f6fd060b07f83857bd44bc6faf33931a69ac8b
tracer-ics2023 <tracer@njuics.org>
2023-12-14 19:12:17 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:12:17 up  3:06,  4 users,  load average: 0.49, 0.41, 0.29

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -21,7 +21,7 @@ int main() {
   assert(ia == (int)(3.2 / -3.2));
   ia =  fixedpt_toint(fixedpt_floor(a));
   assert(ia == 3);
-  ia =  fixedpt_fracpart;
+  ia =  fixedpt_fracpart(b);
   PRINT(ia, -4)
   assert(ia == -4);
   ia =  fixedpt_toint(fixedpt_ceil(a));
