d51dc6ec3fa1e06db6eebe8b93327e1dc48b56f2
tracer-ics2023 <tracer@njuics.org>
2023-12-14 20:00:26 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.6.0-060600-generic #202311151808 SMP PREEMPT_DYNAMIC Thu Nov 16 06:04:00 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:00:26 up  3:54,  4 users,  load average: 0.34, 0.29, 0.27

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include <fixedptc.h>
-#define ASSERT(x, y) assert((int)(x) == (int)(y))
-#define PRINT(x, y) printf("get = %d, want = %d\n", (int)x, (int)(y))
+#define CHECK(x, y)\
+  printf("get = %d, want = %d\n", (int)x, (int)(y));\
+  assert((int)(x) == (int)(y));
 #define INT(x) fixedpt_toint(x)
 #define INIT(x, y) \
   fixedpt a = fixedpt_rconst(x);\
@@ -11,23 +12,31 @@
 
 int main() {
   INIT(3.2, -3.2);
+
   ia =  INT(fixedpt_muli(a, 3));
-  assert(ia == (int)(3.2 * 3));
+  CHEKC(ia , 3.2 * 3);
+
   ia =  INT(fixedpt_mul(a, b));
-  PRINT(ia , (int)(3.2 * -3.2));
-  assert(ia == (int)(3.2 * -3.2));
+  CHEKC(ia , 3.2 * -3.2);
+
   ia =  INT(fixedpt_divi(a, 2));
-  assert(ia == (int)3.2 / 2);
+  CHEKC(ia , 3.2 / 2);
+
   ia =  INT(fixedpt_div(a, b));
-  assert(ia == (int)(3.2 / -3.2));
+  CHEKC(ia , 3.2 / -3.2);
+
   ia =  INT(fixedpt_floor(a));
-  assert(ia == 3);
+  CHEKC(ia , 3);
+
   ia =  INT(fixedpt_floor(b));
-  assert(ia == -4);
+  CHEKC(ia , -4);
+
   ia =  INT(fixedpt_ceil(a));
-  assert(ia == 4);
+  CHEKC(ia , 4);
+
   ia =  INT(fixedpt_ceil(b));
-  assert(ia == -3);
+  CHEKC(ia , -3);
+
   printf("PASS\n");
   return 0;
 }
