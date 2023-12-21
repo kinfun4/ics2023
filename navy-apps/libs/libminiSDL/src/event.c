085b8c5381671dc30668efefcd5f21d0b76655bf
tracer-ics2023 <tracer@njuics.org>
2023-12-21 23:04:50 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:04:50 up 1 day,  2:04, 10 users,  load average: 0.62, 0.81, 0.78

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,8 @@
 #include <NDL.h>
 #include <assert.h>
-#include <SDL.h>
+#include <sdl-event.h>
+#include <stdio.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -15,11 +17,40 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  assert(0);
+  char buf[64];
+  if(NDL_PollEvent(buf, sizeof(buf)) == 0)
+    return 0;
+  if(strncmp(buf, "ku", 2) == 0){
+    ev->key.type = SDL_KEYUP;
+    char name[10];
+    sscanf(buf, "ku %s", name);
+    for(int i=0;i < SDLK_NUM; i++){
+      if(strcmp(name, keyname[i]) == 0){
+        ev->key.keysym.sym = i;
+        return 1;
+      }
+    }
+    assert(0);
+  }
+  if(strncmp(buf, "kd", 2) == 0){
+    ev->key.type = SDL_KEYDOWN;
+    char name[10];
+    sscanf(buf, "kd"" %s", name);
+    for(int i=0;i < SDLK_NUM; i++){
+      if(strcmp(name, keyname[i]) == 0){
+        ev->key.keysym.sym = i;
+        return 1;
+      }
+    }
+    assert(0);
+  }
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  while (!SDL_PollEvent(event)) {
+    continue;
+  }
   return 1;
 }
 
