1db5919d43b2f2aab4e012f13d42c0c95911a17b
tracer-ics2023 <tracer@njuics.org>
2023-12-22 01:00:57 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:00:57 up 1 day,  4:00, 10 users,  load average: 0.57, 0.96, 0.91

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -1,10 +1,10 @@
 #include <assert.h>
-#include <sys/time.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 static int evtdev = -1;
@@ -30,7 +30,8 @@ void NDL_OpenCanvas(int *w, int *h) {
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
-    screen_w = *w; screen_h = *h;
+    screen_w = *w;
+    screen_h = *h;
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
@@ -38,9 +39,11 @@ void NDL_OpenCanvas(int *w, int *h) {
     while (1) {
       // 3 = evtdev
       int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
+      if (nread <= 0)
+        continue;
       buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
+      if (strcmp(buf, "mmap ok") == 0)
+        break;
     }
     close(fbctl);
   }
@@ -49,7 +52,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   assert(read(fd, buf, sizeof(buf)));
   sscanf(buf, "WIDTH:%d\nHEIGHT:%d", &width, &height);
   close(fd);
-  if(*w == 0 && *h == 0){
+  if (*w == 0 && *h == 0) {
     *w = width;
     *h = height;
   }
@@ -57,26 +60,22 @@ void NDL_OpenCanvas(int *w, int *h) {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", O_WRONLY);
-  for(int i = 0 ; i < h ; i++){
+  x = (width - w) / 2;
+  y = (height - h) / 2;
+  for (int i = 0; i < h; i++) {
     lseek(fd, ((y + i) * width + x) * sizeof(uint32_t), SEEK_SET);
     write(fd, pixels + i * w, sizeof(uint32_t) * w);
   }
   close(fd);
 }
 
-void NDL_OpenAudio(int freq, int channels, int samples) {
-}
+void NDL_OpenAudio(int freq, int channels, int samples) {}
 
-void NDL_CloseAudio() {
-}
+void NDL_CloseAudio() {}
 
-int NDL_PlayAudio(void *buf, int len) {
-  return 0;
-}
+int NDL_PlayAudio(void *buf, int len) { return 0; }
 
-int NDL_QueryAudio() {
-  return 0;
-}
+int NDL_QueryAudio() { return 0; }
 
 int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
@@ -85,5 +84,4 @@ int NDL_Init(uint32_t flags) {
   return 0;
 }
 
-void NDL_Quit() {
-}
+void NDL_Quit() {}
