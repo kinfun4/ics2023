70fd536afba3857ccbf39ea6d53d01f85b79f256
tracer-ics2023 <tracer@njuics.org>
2023-12-23 01:11:25 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:11:25 up 14:05, 11 users,  load average: 0.45, 0.51, 0.56

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,7 +30,8 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
   case SYS_exit:
-    halt(a[1]);
+    assert(a[1]);
+    naive_uload(NULL, "/bin/menu");
     break;
   case SYS_yield:
     yield();
