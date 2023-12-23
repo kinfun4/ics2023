88ed53a2458c67fbeb2ac00896e657dbc76bebed
tracer-ics2023 <tracer@njuics.org>
2023-12-23 13:23:46 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:46 up 1 day,  2:17, 12 users,  load average: 0.91, 1.09, 0.94

--- navy-apps/libs/libam/src/input.c
+++ navy-apps/libs/libam/src/input.c
@@ -3,7 +3,9 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   SDL_Event ev;
-  SDL_PollEvent(&ev);
-  kbd->keycode = ev.key.keysym.sym;
-  kbd->keydown = (ev.key.type == SDL_KEYDOWN);
+  if(SDL_PollEvent(&ev)){
+    kbd->keycode = ev.key.keysym.sym;
+    kbd->keydown = (ev.key.type == SDL_KEYDOWN);
+  }
+  else kbd->keycode = 0;
 }
