5cdcb5c8966b022866d1f914abea3e5ef0414b6b
tracer-ics2023 <tracer@njuics.org>
2023-12-23 13:25:40 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:25:40 up 1 day,  2:19, 12 users,  load average: 1.12, 1.09, 0.96

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/am-kernels");
+  naive_uload(NULL, "/bin/menu");
   // load program here
 }
 
