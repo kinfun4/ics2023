82d9686ae2d3589cf3471565b2df0a3b2950ab1a
tracer-ics2023 <tracer@njuics.org>
2023-12-24 20:42:27 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:27 up 2 days,  9:36, 11 users,  load average: 0.61, 0.70, 0.60

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -106,7 +106,6 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
     break;
   default: assert(0);
   }
-    printf("fd = %d, offset = %x\n",fd,  file_offset[fd]);
   assert(file_offset[fd]>=0 && file_offset[fd] <= file_table[fd].size);
   if(file_offset[fd] < 0) file_offset[fd] = 0;
   if(file_offset[fd] > file_table[fd].size) file_offset[fd] = file_table[fd].size;
