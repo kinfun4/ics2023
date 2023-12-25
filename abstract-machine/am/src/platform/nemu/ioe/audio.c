aa57cb93cdf103f4e3af7d0b6646c5c8d71141ac
tracer-ics2023 <tracer@njuics.org>
2023-12-25 19:53:00 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:53:00 up 22:54, 13 users,  load average: 0.53, 0.47, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -15,6 +15,7 @@ static uint32_t bufsize=0;
 
 void __am_audio_init() {
   bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  offset_addr = 0;
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
@@ -23,7 +24,6 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  offset_addr = 0;
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
