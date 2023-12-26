c18bd25d0901420e64160ef68254c055372eed80
tracer-ics2023 <tracer@njuics.org>
2023-12-26 10:07:15 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:07:15 up  9:34, 11 users,  load average: 0.22, 0.19, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/nplayer");
+  naive_uload(NULL, "/bin/nterm");
   // load program here
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,7 +29,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
   case SYS_exit:
     assert(a[1] == 0);
-    naive_uload(NULL, "/bin/menu");
+    naive_uload(NULL, "/bin/nterm");
     break;
   case SYS_yield:
     yield();
