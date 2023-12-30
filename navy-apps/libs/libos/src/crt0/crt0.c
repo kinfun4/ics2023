452a6ce9863a398da905287dd0c599c1e8e9635e
tracer-ics2023 <tracer@njuics.org>
2023-12-30 13:44:48 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:48 up 1 day, 17:41, 10 users,  load average: 0.41, 0.61, 0.63

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   int argc = args[0];
-  char **argv = (char **)args[1];
-  char **envp = (char **)args[argc + 2];
+  char **argv = (char **)(args + 1);
+  char **envp = (char **)(args + argc + 2);
   printf("%p\n", argv);
   printf("%p\n", argv[0]);
   char *empty[] =  {NULL };
