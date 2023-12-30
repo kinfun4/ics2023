453e170b6790b2185757a4bddbaa67e1aaa10338
tracer-ics2023 <tracer@njuics.org>
2023-12-30 15:57:45 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:45 up 1 day, 19:54, 10 users,  load average: 1.12, 0.84, 0.65

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -18,7 +18,7 @@ void cmd_r(const char *buf){
   while (*buf != '\n') filename[i++] = *(buf++);
   filename[i] = '\0';
   printf("%s\n", filename);
-  char **empty = {NULL};
+  char *empty[] = {NULL};
   printf("%p\n", empty);
   int ret = execvp(filename, empty);
   if(ret == -1) sh_printf("filename error!\n");
