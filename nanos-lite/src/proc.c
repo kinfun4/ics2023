4208f3b621b2924e29d0b7bf94ad1db3352b0257
tracer-ics2023 <tracer@njuics.org>
2023-12-30 15:19:29 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:29 up 1 day, 19:15, 10 users,  load average: 0.29, 0.38, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ intptr_t uload(PCB *pcb, const char *filename);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   intptr_t entry = uload(pcb, filename);
   pcb->cp = ucontext(&pcb->as, (Area) { pcb->stack, pcb + 1 }, (void *)entry);
-  printf("%p\n", pcb->cp);
+  // printf("%p\n", pcb->cp);
 
   char *sp = (char *)new_page(PG_PER_STACK);
 
@@ -90,5 +90,6 @@ void init_proc() {
 Context* schedule(Context *prev) {
   current->cp = prev;
   current = (current == &pcb_boot ? &pcb[0] : &pcb[1]);
+  printf("%p\n", current->cp->GPRx);
   return current->cp;
 }
