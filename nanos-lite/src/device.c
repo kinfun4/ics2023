53ef6a1712a0ef3b84d07623fe8c2470678879a5
tracer-ics2023 <tracer@njuics.org>
2024-01-10 19:02:32 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:32 up 2 days, 19:53, 11 users,  load average: 0.94, 0.99, 0.84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,7 +14,7 @@ static const char *keyname[256]
     __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  // yield();
+  yield();
   char *_buf = (char *)buf;
   size_t ret = 0;
   while (ret < len) {
