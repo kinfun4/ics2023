ee9c4a64f317ed97af6238c006394021cf468860
tracer-ics2023 <tracer@njuics.org>
2024-01-10 19:37:03 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:03 up 2 days, 20:27, 11 users,  load average: 3.91, 2.95, 1.72

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -73,8 +73,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
   PCB *p = current == &pcb[0] ? &pcb[1] : &pcb[0];
+  printf("%d\n", __LINE__);
   if(fs_open(filename, 0, 0) == -1)return -2;
-  printf("filename = %s\n", filename);
+  printf("%d\n", __LINE__);
   context_uload(p, filename, argv, envp);
   yield();
   return 0;
