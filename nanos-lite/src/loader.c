f8498030af489fadcccbaca260e555acac0c18e8
tracer-ics2023 <tracer@njuics.org>
2024-01-10 18:41:23 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:41:23 up 2 days, 19:32, 11 users,  load average: 0.33, 0.32, 0.55

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,9 +37,9 @@ static size_t page_load(PCB *pcb, int fd, void *vaddr, size_t file_sz, size_t me
     printf("paddr = %#x, vaddr = %#x, len = %#x\n", _paddr + offset, _vaddr, _len);
     fs_read(fd, _paddr + offset, _len);
     memset(_paddr + offset + _len, 0, PGSIZE - offset - _len);
-    offset = 0;
     vpn += PGSIZE;
     nload += PGSIZE - offset;
+    offset = 0;
   }
 
   while (nload < mem_sz) {
@@ -51,9 +51,9 @@ static size_t page_load(PCB *pcb, int fd, void *vaddr, size_t file_sz, size_t me
     map(&pcb->as, _vaddr, _paddr, 0x7);
     memset(_paddr + offset, 0, _len);
     printf("paddr = %#x, vaddr = %#x, len = %#x\n", _paddr + offset, _vaddr, _len);
-    offset = 0;
     vpn += PGSIZE;
     nload += _len;
+    offset = 0;
   }
 
   return mem_sz;
