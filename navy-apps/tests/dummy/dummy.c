bf3a278f3abf139115ad5d5f966ec32d41636e9e
tracer-ics2023 <tracer@njuics.org>
2024-01-10 11:33:01 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:33:01 up 2 days, 12:23, 11 users,  load average: 3.08, 2.81, 2.39

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -9,7 +9,6 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  printf("%d\n", __LINE__);
-  printf("%d\n", __LINE__);
+  printf("%d 12312412\n", __LINE__);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
