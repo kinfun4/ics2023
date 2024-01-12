e093eb00b1456a94c64173a76ed1b3052fb39ee8
tracer-ics2023 <tracer@njuics.org>
2024-01-12 23:25:58 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:58 up 4 min, 11 users,  load average: 0.54, 0.50, 0.23

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -82,7 +82,7 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
   if (fs_open(filename, 0, 0) == -1)
     return -2;
   context_uload(p, filename, argv, envp);
-  printf("%d\n", __LINE__);
+  printf("%#x\n", p->cp->mepc);
   yield();
   return 0;
 }
