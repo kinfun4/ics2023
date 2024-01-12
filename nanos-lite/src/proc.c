ac81d2e0503441cb871ad2e7624bc87f7ce17e94
tracer-ics2023 <tracer@njuics.org>
2024-01-12 12:06:22 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:06:22 up 1 day, 14:06, 11 users,  load average: 1.15, 0.65, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -105,7 +105,7 @@ Context* schedule(Context *prev) {
   // current = &pcb[0];
   static int cnt = 0;
   if(current == &pcb[0])cnt++;
-  if(cnt == 60) current = &pcb[1], cnt = 0;
+  if(cnt == 30) current = &pcb[1], cnt = 0;
   else current = &pcb[0];
   return current->cp;
 }
