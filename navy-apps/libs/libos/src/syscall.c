2f754559b39e93ac5afed9a99487fdc69f62601a
tracer-ics2023 <tracer@njuics.org>
2024-01-12 23:48:15 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:48:15 up 26 min, 12 users,  load average: 0.44, 0.49, 0.39

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -103,7 +103,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
   int ret = _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
   if(ret<0)errno = -ret;
-  return ret != 0 ? -1 : 0;
+  return 0;
 }
 
 // Syscalls below are not used in Nanos-lite.
