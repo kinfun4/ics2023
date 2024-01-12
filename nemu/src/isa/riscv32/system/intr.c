140b3d3793f00d4eb3ba08ac98d365520bc6e7ab
tracer-ics2023 <tracer@njuics.org>
2024-01-12 11:51:11 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:51:11 up 1 day, 13:51, 11 users,  load average: 0.39, 0.51, 0.51

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -26,7 +26,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
+  static int cnt = 0;
+  cnt++;
   printf("intr = %d, MIE = %d\n", cpu.intr, CSR(MSTATUS) & MIE);
+  assert(cnt<1000);
   if (cpu.intr && CSR(MSTATUS) & MIE) {
     cpu.intr = false;
     return IRQ_TIMER;
