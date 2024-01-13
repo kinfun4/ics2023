990da5cf819f4c279d5cde7b787b3558012e05d0
tracer-ics2023 <tracer@njuics.org>
2024-01-13 10:51:40 +0800

    >  compile NEMU 221900156 韩加瑞 Linux han-HP-ZHAN-A-G6 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:40 up 11:29, 12 users,  load average: 0.24, 0.34, 0.49

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,7 +19,7 @@ Context* __am_irq_handle(Context *c) {
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
-    printf("2:%#x\n", c->mepc);
+    printf("2:event = %d, mepc = %#x\n", ev.event ,c->mepc);
     assert(c != NULL);
   }
 
